cdb1988ae56ba7c1c943eef485d3e5a9eed42917
Wojtek Szymczyk <wojciechszymczyk9@gmail.com>
2023-01-09 12:35:09 +0100

    fixed-fixed

--- .eleventy.js
+++ .eleventy.js
@@ -6,8 +6,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/admin");
 
   eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
-  })
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     dir: {
